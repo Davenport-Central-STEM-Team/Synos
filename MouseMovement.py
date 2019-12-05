f9906482c39dc867f491ed43a62be967b8443621
nick <vazqueznicholas1@gmail.com>
2019-12-05 14:50:22 -0600

    Mouse control works, functions for left and right click

--- MouseMovement.py
+++ MouseMovement.py
@@ -1,49 +1,19 @@
 #!/usr/bin/env python3
+import time
+
 NULL_CHAR = chr(0)
 
 
-# def write_report(report):
-#     with open('/dev/hidg0', 'rb+') as fd:
-#         fd.write(report.encode())
-#
-# # Press a
-# write_report(NULL_CHAR*2+chr(4)+NULL_CHAR*5)
-# # Release keys
-# write_report(NULL_CHAR*8)
-# # Press SHIFT + a = A
-# write_report(chr(32)+NULL_CHAR+chr(4)+NULL_CHAR*5)
-#
-# # Press b
-# write_report(NULL_CHAR*2+chr(5)+NULL_CHAR*5)
-# # Release keys
-# write_report(NULL_CHAR*8)
-# # Press SHIFT + b = B
-# write_report(chr(32)+NULL_CHAR+chr(5)+NULL_CHAR*5)
-#
-# # Press SPACE key
-# write_report(NULL_CHAR*2+chr(44)+NULL_CHAR*5)
-#
-# # Press c key
-# write_report(NULL_CHAR*2+chr(6)+NULL_CHAR*5)
-# # Press d key
-# write_report(NULL_CHAR*2+chr(7)+NULL_CHAR*5)
-#
-# # Press RETURN/ENTER key
-# write_report(NULL_CHAR*2+chr(40)+NULL_CHAR*5)
-#
-# # Press e key
-# write_report(NULL_CHAR*2+chr(8)+NULL_CHAR*5)
-# # Press f key
-# write_report(NULL_CHAR*2+chr(9)+NULL_CHAR*5)
-#
-# # Release all keys
-# write_report(NULL_CHAR*8)
-def signed16(value):
-    return -(value & 0x80000000) | (value & 0x7fffffff)
+def write_report(report):
+    with open('/dev/hidg0', 'rb+') as fd:
+        fd.write(report.encode())
+
+def write_null():
+    write_report(NULL_CHAR*3)
 
 
 def pixels_to_hex(pixels):
-    print(hex(pixels))
+    return hex(pixels)
 
 
 def posIntToHex(pixels):
@@ -52,12 +22,21 @@ def posIntToHex(pixels):
     else:
         raise ArithmeticError("Number is not positive!")
 
-# print(signed16(int('0x7f', 16)))
-# pixels_to_hex(-127)
-# print(chr(-127) == '\xFFFFFF81')
-try:
-    print(posIntToHex(-127))
-except ArithmeticError:
-    print("You done fucked up.")
-except:
-    print("IDEK what u did, bro")
+
+def leftClick():
+    write_report(chr(1) + NULL_CHAR * 2)
+    time.sleep(0.1)
+    write_null()
+
+
+def rightClick():
+    write_report(chr(2) + NULL_CHAR * 2)
+    time.sleep(0.1)
+    write_null()
+
+
+while True:
+    leftClick()
+    time.sleep(0.5)
+    rightClick()
+    time.sleep(0.5)
