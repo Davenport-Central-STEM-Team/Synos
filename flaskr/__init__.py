18fe472da5d34b34a261f58d13ab247f6e43edc6
nick <vazqueznicholas1@gmail.com>
2020-01-23 14:03:17 -0600

    Flask tutorial code added

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,6 +3,11 @@ from flask import Flask
 
 
 def create_app(test_config=None):
+    """
+    Flask App Factory
+
+    Creates instance of flask web server with SQLite and relative config files
+    """
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
@@ -27,5 +32,15 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
